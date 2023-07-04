8af847fecbca4535afa55aaa5902610b0e9f372b
ramveersingharaon <ramveersingharaon@gmail.com>
2023-07-04 14:15:18 +0530

    upload dist folder

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({ baseURL: "https://ss-patharia-public-school-k4tb.onrender.com/" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('token')) {
